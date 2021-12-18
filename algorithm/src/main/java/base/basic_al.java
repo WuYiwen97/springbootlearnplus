7271ddf8535f4775ca985798c2b887afb4dd4fcb
wuyiw <wuyiwen91@163.com>
2021-12-18 08:40:52 +0800

    周日学习内容

--- algorithm/src/main/java/base/basic_al.java
+++ algorithm/src/main/java/base/basic_al.java
@@ -1,5 +1,8 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class basic_al {
 
     //计算2个数的最小公倍数
@@ -18,5 +21,31 @@ public class basic_al {
     public static int getSM(int a,int b ){
         //这个好复杂啊 ™
         return 1;
+    }
+
+    //质数
+    public static boolean getZhishu(int number){
+        if (number == 1 || number ==2)
+            return true;
+        for (int i = 2;i<number;i++){
+            if (number%i ==0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //整数位数
+    public static int getWeishu(int number) {
+        int weishu = 1;
+        while (number / 10 !=0){
+            weishu+=1;
+            number=number / 10;
+        }
+        return weishu;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getWeishu(9999));
     }
 }
